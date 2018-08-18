c42d142218afd9130c2fe7fd8f698ad1b85a99f4
zhen <286339409@qq.com>
2018-08-18 18:52:43 +0800

    fox

--- controller/weixin/weixin.go
+++ controller/weixin/weixin.go
@@ -32,7 +32,55 @@ type WeixinTextData struct {
 	Content      string   `xml:"Content"`
 }
 
+type WeixinBaseData struct {
+	XMLName      xml.Name `xml:"xml"`
+	ToUserName   string   `xml:"ToUserName"`
+	FromUserName string   `xml:"FromUserName"`
+	CreateTime   int64    `xml:"CreateTime"`
+	MsgType      string   `xml:"MsgType"`
+}
 
+type WeixinImgData struct {
+	WeixinBaseData
+	Image Image `xml:"Image"`
+}
+
+type Image struct {
+	MediaId string `xml:"MediaId"`
+}
+
+type WeixinMusicData struct {
+	WeixinBaseData
+	Music Music `xml:"Music"`
+}
+
+type Music struct {
+	Title        string `xml:"Title"`
+	Description  string `xml:"Description"`
+	MusicUrl     string `xml:"MusicUrl"`
+	HQMusicUrl   string `xml:"HQMusicUrl"`
+	ThumbMediaId string `xml:"ThumbMediaId"`
+}
+
+type WeixinNewsData struct {
+	WeixinBaseData
+	ArticleCount string `xml:"ArticleCount"`
+	Articles     Item   `xml:"Articles"`
+}
+
+type Item struct {
+	XMLName    xml.Name `xml:"Articles"`
+	News []News `xml:"item"`
+}
+type News struct {
+	XMLName    xml.Name `xml:"item"`
+	Title       string `xml:"Title"`
+	Description string `xml:"Description"`
+	PicUrl      string `xml:"PicUrl"`
+	Url         string `xml:"Url"`
+}
+
+//6590986134459777000
 func WeixinAction(c *gin.Context) {
 
 	if c.Request.Method == "GET" {
@@ -58,22 +106,32 @@ func WeixinAction(c *gin.Context) {
 
 		var key model.KeywordsReply
 		if err := model.FindOneByFiled("keywords_reply", "key_word", weixinPostParam.Content, &key); err != nil {
-			return
+			key.MsgType = 9
 		}
 
 		switch key.MsgType {
 		case model.KeywordsReplyMsgText:
 			var data = WeixinTextData{
-				ToUserName:   fromUserName,
-				FromUserName: toUserName,
+				ToUserName:   toUserName,
+				FromUserName: fromUserName,
 				Content:      key.Value,
 				MsgType:      "text",
 				CreateTime:   time.Now().Unix(),
 			}
 			d, _ := xml.Marshal(&data)
+			fmt.Printf(string(d))
 			c.Writer.WriteString(string(d))
 			break
 		case model.KeywordsReplyMsgImage:
+			var data = WeixinImgData{}
+			data.ToUserName = toUserName
+			data.FromUserName = fromUserName
+			data.MsgType = "image"
+			data.CreateTime = time.Now().Unix()
+			data.Image.MediaId = "100000001"
+			d, _ := xml.Marshal(&data)
+			fmt.Printf(string(d))
+			c.Writer.WriteString(string(d))
 			break
 		case model.KeywordsReplyMsgVoice:
 			break
@@ -82,11 +140,35 @@ func WeixinAction(c *gin.Context) {
 		case model.KeywordsReplyMsgMusic:
 			break
 		case model.KeywordsReplyMsgNews:
+			subs, err := model.FindKeywordsReplyNewsSubByReplyId(true, key.ID)
+			if err != nil {
+				break
+			}
+			var data = WeixinNewsData{}
+			data.ToUserName = toUserName
+			data.FromUserName = fromUserName
+			data.MsgType = "news"
+			data.CreateTime = time.Now().Unix()
+			data.ArticleCount = "3"
+
+			var items Item
+			for _, value := range *subs {
+				var new News
+				new.Title = value.Title
+				new.Description = value.Description
+				new.PicUrl = value.PicUrl
+				new.Url = value.Url
+				items.News=append(items.News,new)
+			}
+			data.Articles=items
+			d, _ := xml.Marshal(&data)
+			fmt.Printf(string(d))
+			c.Writer.WriteString(string(d))
 			break
 		default:
 			var data = WeixinTextData{
-				ToUserName:   fromUserName,
-				FromUserName: toUserName,
+				ToUserName:   toUserName,
+				FromUserName: fromUserName,
 				Content:      "what?",
 				MsgType:      "text",
 				CreateTime:   time.Now().Unix(),
@@ -95,8 +177,6 @@ func WeixinAction(c *gin.Context) {
 			c.Writer.WriteString(string(d))
 			break
 		}
-
-
 	}
 
 }
